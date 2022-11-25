b136159ace8ab12e9176014af53ae589ace59951
Eemil <eemil.arola@gmail.com>
2022-11-25 12:22:10 +0200

    kahden eri tuotteen hintojen summa toimii korissa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_hinta_sama_kuin_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        self.assertEqual(self.kori.hinta(), 5)
