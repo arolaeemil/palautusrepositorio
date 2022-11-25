997b41d6e7f544a368754bbb53ea0f5595b37ece
Eemil <eemil.arola@gmail.com>
2022-11-25 12:17:03 +0200

    yhden tuotteen lisäys lisää yhden tavaran koriin

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -20,8 +20,7 @@ class Ostoskori:
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        self.ostoslista.append(Ostos(lisattava))
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
