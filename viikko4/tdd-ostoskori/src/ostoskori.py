3b2d5978afc45aa01be75f3ec8cdc9d7b976bbf5
Eemil <eemil.arola@gmail.com>
2022-11-25 12:19:28 +0200

    lisatyn yhden tuotteen hinta toimii oikein korissa

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -16,7 +16,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        totalhinta = 0
+        for ostos in self.ostoslista:
+            totalhinta = totalhinta + ostos.hinta()
+        return totalhinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
