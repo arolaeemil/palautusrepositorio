cd61a1d69522de07679898b47eee6d31f2c39446
Eemil <eemil.arola@gmail.com>
2022-12-06 19:32:05 +0200

    tehtävät ehkä tehty viikko 6

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,13 +1,13 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan, Not, Or, QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
+    """matcher = And(
         HasAtLeast(5, "goals"),
         HasAtLeast(5, "assists"),
         PlaysIn("PHI")
@@ -16,6 +16,56 @@ def main():
     for player in stats.matches(matcher):
         print(player)
 
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+    matcher = And(
+    HasFewerThan(1, "goals"),
+    PlaysIn("NYR")
+    )
+
+    matcher = And(
+    Not(HasAtLeast(1, "goals")),
+    PlaysIn("NYR")
+    )
+
+    matcher = Or(
+    HasAtLeast(45, "goals"),
+    HasAtLeast(70, "assists")
+    )
+
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
+    )"""
+
+    query = QueryBuilder()
+    matcher = query.build()
+    matcher = query.playsIn("NYR").build()
+    matcher = query.playsIn("NYR").hasAtLeast(10, "goals").hasFewerThan(20, "goals") .build()
+    m1 = (
+    query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
+    )
+
+    m2 = (
+    query
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+
+    matcher = query.oneOf(m1, m2).build()
+
+    for player in stats.matches(matcher):
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,78 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+#Omat
+
+class All:
+    def __init__(self):
+        self._value = True
+
+    def test(self, player):
+        return self._value
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+class Not:
+    def __init__(self, ehto):
+        self._ehto = ehto
+
+    def test(self, player):
+        tulos = self._ehto.test(player)
+        if tulos == True:
+            return False
+        else:
+            return True
+
+class Or:
+    #def __init__(self, ehto1, ehto2 = None, ehto3 = None):
+    #olisi ollut aika rumaa ehtolauseella
+    def __init__(self, *args):
+        self._ehdot = []
+        i = 0
+        while i < len(args): 
+            self._ehdot.append(args[i])
+            i = i + 1
+        #self._ehdot.append(ehto1)
+        #if ehto2 != None:
+            #self._ehdot.append(ehto2)
+            #if ehto3 != None:
+                #self._ehdot.append(ehto3)
+
+    def test(self, player):
+        for ehto in self._ehdot:
+            if ehto.test(player) == True:
+                return True
+        return False
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self.matcher_olio = matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self.matcher_olio,PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.matcher_olio,HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.matcher_olio,HasFewerThan(value, attr)))
+
+    def oneOf(self, *args):
+        i = 0
+        while i < len(args):
+            #palautus = QueryBuilder(Or(And(self.matcher_olio, args[i])))
+            palautus = QueryBuilder(Or(And(args[i]), Not(self.matcher_olio)))
+            i = i + 1
+        return palautus
+
+    def build(self):
+        return self.matcher_olio
